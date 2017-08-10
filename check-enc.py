6f94522a1898f7ca476be2eb4b812793fbc5b5cc
Jonathan Elchison <JElchison@Gmail.com>
2017-08-10 12:06:49 -0400

    PEP8

--- check-enc.py
+++ check-enc.py
@@ -1,23 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
-# Name:  checkenc.py
+# Name:  check-enc.py
 # Purpose:  Attempts to decode/encode unknown data
 # History:
 #    v0.1 - Ben Britton       - Initial encodings
 #    v0.2 - Jonathan Elchison - Code organization
 #                             - Added exception handling
-#                             - Added scoring of results based on letter frequencies 
+#                             - Added scoring of results based on letter frequencies
 #                             - Added reporting of results
 #                             - Added support for second pass
 #                             - Added support for params, help text
 #                             - Added support for yenc
 
-import binascii
-import sys
-import codecs
 import base64
-import math
+import binascii
+import codecs
 import copy
+import math
+import sys
 import yenc
 
 
@@ -26,37 +26,36 @@ numResults = 20
 
 # create table of frequency logarithms
 logs = dict()
-logs[ 'a' ] = math.log( 0.11602, 2 )
-logs[ 'b' ] = math.log( 0.04702, 2 )
-logs[ 'c' ] = math.log( 0.03511, 2 )
-logs[ 'd' ] = math.log( 0.02670, 2 )
-logs[ 'e' ] = math.log( 0.02000, 2 )
-logs[ 'f' ] = math.log( 0.03779, 2 )
-logs[ 'g' ] = math.log( 0.01950, 2 )
-logs[ 'h' ] = math.log( 0.07232, 2 )
-logs[ 'i' ] = math.log( 0.06286, 2 )
-logs[ 'j' ] = math.log( 0.00631, 2 )
-logs[ 'k' ] = math.log( 0.00690, 2 )
-logs[ 'l' ] = math.log( 0.02705, 2 )
-logs[ 'm' ] = math.log( 0.04374, 2 )
-logs[ 'n' ] = math.log( 0.02365, 2 )
-logs[ 'o' ] = math.log( 0.06264, 2 )
-logs[ 'p' ] = math.log( 0.02545, 2 )
-logs[ 'q' ] = math.log( 0.00173, 2 )
-logs[ 'r' ] = math.log( 0.01653, 2 )
-logs[ 's' ] = math.log( 0.07755, 2 )
-logs[ 't' ] = math.log( 0.16671, 2 )
-logs[ 'u' ] = math.log( 0.01487, 2 )
-logs[ 'v' ] = math.log( 0.00619, 2 )
-logs[ 'w' ] = math.log( 0.06661, 2 )
-logs[ 'x' ] = math.log( 0.00005, 2 )
-logs[ 'y' ] = math.log( 0.01620, 2 )
-logs[ 'z' ] = math.log( 0.00050, 2 )
+logs['a'] = math.log(0.11602, 2)
+logs['b'] = math.log(0.04702, 2)
+logs['c'] = math.log(0.03511, 2)
+logs['d'] = math.log(0.02670, 2)
+logs['e'] = math.log(0.02000, 2)
+logs['f'] = math.log(0.03779, 2)
+logs['g'] = math.log(0.01950, 2)
+logs['h'] = math.log(0.07232, 2)
+logs['i'] = math.log(0.06286, 2)
+logs['j'] = math.log(0.00631, 2)
+logs['k'] = math.log(0.00690, 2)
+logs['l'] = math.log(0.02705, 2)
+logs['m'] = math.log(0.04374, 2)
+logs['n'] = math.log(0.02365, 2)
+logs['o'] = math.log(0.06264, 2)
+logs['p'] = math.log(0.02545, 2)
+logs['q'] = math.log(0.00173, 2)
+logs['r'] = math.log(0.01653, 2)
+logs['s'] = math.log(0.07755, 2)
+logs['t'] = math.log(0.16671, 2)
+logs['u'] = math.log(0.01487, 2)
+logs['v'] = math.log(0.00619, 2)
+logs['w'] = math.log(0.06661, 2)
+logs['x'] = math.log(0.00005, 2)
+logs['y'] = math.log(0.01620, 2)
+logs['z'] = math.log(0.00050, 2)
 
 
-def PrintHelp():
-    print """
-Purpose:  Attempts to decode/encode unknown data
+def print_help():
+    print """Purpose:  Attempts to decode/encode unknown data
 
 Prereq:   Python package 'yenc'.  On Debian systems, this can be installed
           using `sudo apt-get install python-yenc`
@@ -71,16 +70,16 @@ Usage:    checkenc.py [-d|-e] [-1] [-a] [-v] -f <file>
 Options:  -d  Decode data (default)
 
           -e  Encode data (if absent, -d is assumed)
-          
+
           -1  Only performs one decoding/encoding pass on data (if absent,
               default behavior is to attempt all permutations of any two
               encodings).  This will decrease running time for large data
               sets.
-          
+
           -a  Display all results (if absent, only top %d are displayed)
-          
+
           -v  Verbose mode
-          
+
 Output:   One row is displayed for each result.  There are three columns in
           each row:
               1)  Score.  higher (more positive) is better.  The score will
@@ -89,232 +88,232 @@ Output:   One row is displayed for each result.  There are three columns in
                   is a Caesar cipher with each byte incremented by XXX.
               3)  Result after de/encoding.  Special characters will be escaped
                   appropriately.
-    """ % numResults    
+    """ % numResults
 
 
-def CalculateEntropy( data ):
+def calculate_entropy(data):
     score = 0
     for char in data:
         try:
-            score += logs[ char.lower() ]
+            score += logs[char.lower()]
         except:
             # not a character.  subtract arbitrary amount from score.
             score -= 100
     return score
 
 
-def ParseData( inData, bDecode, bCaesar ):
-    answers = list()
-    
-    if bDecode is True:
-        
-        for name in ( 'hex_codec', 'base64_codec', 'rot_13', 'utf_8', 'utf_16_be', 'utf_16_le', 'bz2', 'zip', 'idna', 'palmos', 'punycode', 'quopri_codec', 'utf_7' ):
+def parse_data(data, my_decode_flag, my_caesar_flag):
+    my_answers = list()
+
+    if my_decode_flag is True:
+
+        for name in ('hex_codec', 'base64_codec', 'rot_13', 'utf_8', 'utf_16_be', 'utf_16_le', 'bz2', 'zip', 'idna', 'palmos', 'punycode', 'quopri_codec', 'utf_7'):
             try:
-                result = inData.decode( name )
-                if bVerbose:
+                result = data.decode(name)
+                if verbose_flag:
                     print "%s succeeded" % name
-                answers.append( [ name, repr( result ) ] )
+                my_answers.append([name, repr(result)])
             except Exception as ex:
-                if bVerbose:
-                    print "%s FAILED: %s" % ( name, ex )
-    
+                if verbose_flag:
+                    print "%s FAILED: %s" % (name, ex)
+
+        name = 'base32'
         try:
-            name = 'base32'
-            result = base64.b32decode( inData )
-            if bVerbose:
+            result = base64.b32decode(data)
+            if verbose_flag:
                 print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
+            my_answers.append([name, repr(result)])
         except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
-    
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+        name = 'uuencode'
         try:
-            name = 'uuencode'
-            result = binascii.a2b_uu( inData )
-            if bVerbose:
+            result = binascii.a2b_uu(data)
+            if verbose_flag:
                 print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
+            my_answers.append([name, repr(result)])
         except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
-            
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+        name = 'quotable'
         try:
-            name = 'quotable'
-            result = binascii.a2b_qp( inData )
-            if bVerbose:
+            result = binascii.a2b_qp(data)
+            if verbose_flag:
                 print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
+            my_answers.append([name, repr(result)])
         except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
-            
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+        name = 'binhex4'
         try:
-            name = 'binhex4'
-            result = binascii.rledecode_hqx( binascii.a2b_hqx( inData ) )
-            if bVerbose:
+            result = binascii.rledecode_hqx(binascii.a2b_hqx(data))
+            if verbose_flag:
                 print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
+            my_answers.append([name, repr(result)])
         except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
-        
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+        name = 'yenc'
         try:
-            name = 'yenc'
             decoder = yenc.Decoder()
-            decoder.feed( inData )
-            result = decoder.getDecoded() 
-            if bVerbose:
+            decoder.feed(data)
+            result = decoder.getDecoded()
+            if verbose_flag:
                 print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
+            my_answers.append([name, repr(result)])
         except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
 
     else:
-        
-        for name in ( 'hex_codec', 'base64_codec', 'rot_13', 'utf_8', 'utf_16_be', 'utf_16_le', 'bz2', 'zip', 'idna', 'palmos', 'punycode', 'quopri_codec', 'utf_7' ):
-            try:
-                result = inData.encode( name )
-                if bVerbose:
-                    print "%s succeeded" % name
-                answers.append( [ name, repr( result ) ] )
-            except Exception as ex:
-                if bVerbose:
-                    print "%s FAILED: %s" % ( name, ex )
-    
-        try:
-            name = 'base32'
-            result = base64.b32encode( inData )
-            if bVerbose:
-                print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
-        except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
-    
-        try:
-            name = 'uuencode'
-            result = binascii.b2a_uu( inData )
-            if bVerbose:
-                print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
-        except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
-            
-        try:
-            name = 'quotable'
-            result = binascii.b2a_qp( inData )
-            if bVerbose:
-                print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
-        except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
-            
-        try:
-            name = 'binhex4'
-            result = binascii.b2a_hqx( binascii.rlecode_hqx( inData ) )
-            if bVerbose:
-                print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
-        except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
-        
-        try:
-            name = 'yenc'
-            encoder = yenc.Encoder()
-            encoder.feed( inData )
-            result = encoder.getEncoded() 
-            if bVerbose:
-                print "%s succeeded" % name
-            answers.append( [ name, repr( result ) ] )
-        except Exception as ex:
-            if bVerbose:
-                print "%s FAILED: %s" % ( name, ex )
 
-    if bCaesar:
-        for i in range( 1, 256 ):
-            answers.append( [ "Caesar_%d" % i, repr( ''.join( [ chr( ( ord( c ) + i ) % 256 ) for c in inData ] ) ) ] )
-        
-    return answers
+        for name in ('hex_codec', 'base64_codec', 'rot_13', 'utf_8', 'utf_16_be', 'utf_16_le', 'bz2', 'zip', 'idna', 'palmos', 'punycode', 'quopri_codec', 'utf_7'):
+            try:
+                result = data.encode(name)
+                if verbose_flag:
+                    print "%s succeeded" % name
+                my_answers.append([name, repr(result)])
+            except Exception as ex:
+                if verbose_flag:
+                    print "%s FAILED: %s" % (name, ex)
+
+        name = 'base32'
+        try:
+            result = base64.b32encode(data)
+            if verbose_flag:
+                print "%s succeeded" % name
+            my_answers.append([name, repr(result)])
+        except Exception as ex:
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+        name = 'uuencode'
+        try:
+            result = binascii.b2a_uu(data)
+            if verbose_flag:
+                print "%s succeeded" % name
+            my_answers.append([name, repr(result)])
+        except Exception as ex:
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+        name = 'quotable'
+        try:
+            result = binascii.b2a_qp(data)
+            if verbose_flag:
+                print "%s succeeded" % name
+            my_answers.append([name, repr(result)])
+        except Exception as ex:
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+        name = 'binhex4'
+        try:
+            result = binascii.b2a_hqx(binascii.rlecode_hqx(data))
+            if verbose_flag:
+                print "%s succeeded" % name
+            my_answers.append([name, repr(result)])
+        except Exception as ex:
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+        name = 'yenc'
+        try:
+            encoder = yenc.Encoder()
+            encoder.feed(data)
+            result = encoder.getEncoded()
+            if verbose_flag:
+                print "%s succeeded" % name
+            my_answers.append([name, repr(result)])
+        except Exception as ex:
+            if verbose_flag:
+                print "%s FAILED: %s" % (name, ex)
+
+    if my_caesar_flag:
+        for i in range(1, 256):
+            my_answers.append(["Caesar_%d" % i, repr(''.join([chr((ord(c) + i) % 256) for c in data]))])
+
+    return my_answers
 
 
 # initialize parameters
-bDecode = True
-bVerbose = False
-isFile = False
-twoPasses = True
-printAll = False
-inData = ''
-inFile = ''
+decode_flag = True
+verbose_flag = False
+is_file = False
+two_passes = True
+print_all = False
+in_data = ''
+in_file = ''
+in_filename = ''
 
-
-if len( sys.argv ) == 1:
-    PrintHelp()
+if len(sys.argv) == 1:
+    print_help()
     exit()
 
-for arg in sys.argv[ 1 : ]:
+for arg in sys.argv[1:]:
     if arg == '-e':
-        bDecode = False
+        decode_flag = False
     elif arg == '-d':
-        bDecode = True
+        decode_flag = True
     elif arg == '-v':
-        bVerbose = True
+        verbose_flag = True
     elif arg == '-f':
-        isFile = True
+        is_file = True
     elif arg == '-1':
-        twoPasses = False
+        two_passes = False
     elif arg == '-a':
-        printAll = True
+        print_all = True
     elif arg == '-h':
-        PrintHelp()
+        print_help()
         exit()
     else:
-        if isFile:
-            inFilename = arg
+        if is_file:
+            in_filename = arg
         else:
-            inData = arg
+            in_data = arg
 
-if isFile is True:
-    inFile = open( inFilename, 'r' )
-    inData = inFile.read()
-    inFile.close()
-    
+if is_file is True:
+    in_file = open(in_filename, 'r')
+    in_data = in_file.read()
+    in_file.close()
+
 # do operation
-answers = ParseData( inData, bDecode, True )
+answers = parse_data(in_data, decode_flag, True)
 
-if twoPasses:
+if two_passes:
     print "First pass complete.  Now trying permutating each result..."
-    # try "2-dimensional" operation (perform ParseData() on each answer)
-    answers_orig = copy.copy( answers )
+    # try "2-dimensional" operation (perform parse_data() on each answer)
+    answers_orig = copy.copy(answers)
     for answer in answers_orig:
         # only do Caesar once
-        if ( answer[ 0 ].find( 'Caesar' ) == -1 ):
-            newAnswers = ParseData( answer[ 1 ], bDecode, True )
+        if answer[0].find('Caesar') == -1:
+            newAnswers = parse_data(answer[1], decode_flag, True)
         else:
-            newAnswers = ParseData( answer[ 1 ], bDecode, False )
+            newAnswers = parse_data(answer[1], decode_flag, False)
         for newAnswer in newAnswers:
-            newAnswer[ 0 ] = "%s + %s" % ( answer[ 0 ], newAnswer[ 0 ] )
-        answers.extend( newAnswers )
+            newAnswer[0] = "%s + %s" % (answer[0], newAnswer[0])
+        answers.extend(newAnswers)
 
-if bVerbose:
+if verbose_flag:
     print '======================================================================'
 
 for answer in answers:
     try:
-        answer.append( CalculateEntropy( answer[ 1 ] ) )
+        answer.append(calculate_entropy(answer[1]))
     except:
         # this is basically disqualified
-        answer.append( -10000 )
-    
-sortedAnswers = sorted( answers, key=lambda answer: answer[ 2 ] )
-if printAll:
-    print "All %d results, worst to best:" % len( sortedAnswers )
+        answer.append(-10000)
+
+sortedAnswers = sorted(answers, key=lambda my_answer: my_answer[2])
+if print_all:
+    print "All %d results, worst to best:" % len(sortedAnswers)
     for answer in sortedAnswers:
-        print "%.2f <%s> %s" % ( answer[ 2 ], answer[ 0 ], answer[ 1 ] )
-else:        
+        print "%.2f <%s> %s" % (answer[2], answer[0], answer[1])
+else:
     print "Top %d results, worst to best:" % numResults
-    for answer in sortedAnswers[ len( sortedAnswers ) - numResults + 1 : ]:
-        print "%.2f <%s> %s" % ( answer[ 2 ], answer[ 0 ], answer[ 1 ] )
+    for answer in sortedAnswers[len(sortedAnswers) - numResults + 1:]:
+        print "%.2f <%s> %s" % (answer[2], answer[0], answer[1])
